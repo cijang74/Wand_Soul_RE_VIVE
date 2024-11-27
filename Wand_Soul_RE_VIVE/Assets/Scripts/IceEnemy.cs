e828b6336ed9cf03a1d89c24240d734eaea85786
toyede <toyede@naver.com>
2024-11-28 03:52:11 +0900

    근거리 몬스터 구현
    얼음, 번개 구현완료

---
+++ Wand_Soul_RE_VIVE/Assets/Scripts/IceEnemy.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceEnemy : Enemy
+{
+    Rigidbody2D rd2d;
+    CapsuleCollider2D EnemyCapsule;
+
+    [SerializeField] float findDistance = 10f;
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float attackDistance = 2f;
+    [SerializeField] float attackCooldown = 2f; // 공격 애니메이션 재생 시간
+    [SerializeField] float idleTimeAfterAttack = 3f; // 공격 속도
+
+    private Transform target;
+    private Animator animator;
+
+    private bool isAttacking = false;
+    private bool canAttack = true; // 공격 가능 상태 플래그
+
+    IceEnemy()
+    {
+        hp = 15;
+    }
+
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        EnemyCapsule = GetComponent<CapsuleCollider2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+    }
+
+    private void StopMoving()
+    {
+        rd2d.velocity = Vector2.zero;
+    }
+
+    private void MoveTowardPlayer()
+    {
+        float moveDirection = target.position.x > transform.position.x ? 1 : -1;
+        Vector2 enemyVelocity = new Vector2(moveSpeed * moveDirection, rd2d.velocity.y);
+        rd2d.velocity = enemyVelocity;
+
+        if (moveDirection < 0)
+            FlipEnemyFacing();
+        else if (moveDirection > 0)
+            FlipEnemyFacing();
+    }
+
+    private IEnumerator AttackPlayer()
+    {
+        isAttacking = true;
+        canAttack = false; // 일정 시간 동안 공격 불가능
+
+        StopMoving(); // 공격 중에는 멈춤
+        animator.SetTrigger("isAttack");
+
+        yield return new WaitForSeconds(attackCooldown);
+
+        animator.ResetTrigger("isAttack");
+
+        isAttacking = false; // 공격 애니메이션이 끝난 직후 isAttacking을 false로 설정
+
+        // idle 또는 이동 상태로 전환
+        yield return new WaitForSeconds(idleTimeAfterAttack);
+
+        canAttack = true; // 다시 공격 가능
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().Damage(5);
+        }
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rd2d.velocity.x)), 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Die();
+            return;
+        }
+        if (target == null)
+        {
+            return;
+        }
+
+        float distanceToPlayer = Vector2.Distance(transform.position, target.position);
+
+        if (distanceToPlayer > findDistance)
+        {
+            StopMoving();
+        }
+        else if (distanceToPlayer <= attackDistance && canAttack)
+        {
+            if (!isAttacking)
+            {
+                StartCoroutine(AttackPlayer());
+            }
+        }
+        else if (!isAttacking) // 공격 중이 아닐 때만 이동
+        {
+            MoveTowardPlayer();
+        }
+    }
+}
---
+++ Wand_Soul_RE_VIVE/Assets/Scripts/JH_Monster/Projectile.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // 화살 발사 스크립트
+    [SerializeField] private float moveSpeed = 22f; // 화살속도
+    [SerializeField] private GameObject particleOnHitPrefabVFX;
+    [SerializeField] private bool isEnemyProjectile = false;
+    [SerializeField] private float projectileRange = 10f;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        MoveProjectile();
+        DetectFireDistance();
+    }
+
+    public void UpdateProjectileRange(float projectileRange)
+    {
+        // 해당 투사체의 사정거리를 설정하는 함수
+        this.projectileRange = projectileRange;
+    }
+
+    public void UpdateMoveSpeed(float moveSpeed)
+    {
+        // 해당 투사체의 사정거리를 설정하는 함수
+        this.moveSpeed = moveSpeed;
+    }
+
+    /* 잠시 주석 처리 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+        Indestructible indestructible = other.gameObject.GetComponent<Indestructible>();
+        PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
+
+        if ((enemyHealth || indestructible || player) && !other.isTrigger)
+        // 만약 트리거 콜라이더와 접촉한것이 적, 또는 삭제 가능하지 않은것, 트리거가 아닌 것 이라면..
+        {
+            if ((player && isEnemyProjectile) || (enemyHealth && !isEnemyProjectile))
+            // 플레이어가 적탄환에 맞았을 때 또는 적탄환에 적이 적이 맞은게 아니라면
+            // 즉, 오직 총알은 플레이어만 닿았을때 삭제되게 함.
+            {
+                player?.TakeDamage(1, transform);
+                Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation); // 삭제 파티클
+                Destroy(gameObject);
+            }
+
+            else if (!other.isTrigger && indestructible) // 지형이나 제거 불가능한 오브젝트와 부딪혔을시
+            {
+                Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation); // 삭제 파티클
+                Destroy(gameObject);
+            }
+
+        }
+    }
+    */
+
+    private void DetectFireDistance()
+    {
+        // 사정거리를 벗어나면 화살 인스턴스를 삭제시키는 메서드
+
+        if (Vector3.Distance(transform.position, startPosition) > projectileRange)
+        // 현재 위치와 처음 쐈을때 위치간의 거리를 사정거리와 비교
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void MoveProjectile()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        // x축 기준 정면방향으로 이동. 
+    }
+}
