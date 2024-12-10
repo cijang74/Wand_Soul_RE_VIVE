95f6dd766b61b8e3382f8f97859c89391edbcca1
Cho Hyeon Ho <chohh0914@naver.com>
2024-12-10 21:36:53 +0900

    불 얼음 공격 추가
    불과 얼음의 일반공격과 강화공격 추가 완료

---
+++ Wand_Soul_RE_VIVE/Assets/TeamProject/Scripts/CHH/FireBall.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour, IWeapon
+{
+    [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject fireBallPrefab;
+    [SerializeField] private Transform fireBallSpawnPoint;
+
+    private Animator myAnimator;
+
+    readonly int FIRE_HASH = Animator.StringToHash("Fire");
+    // 읽기전용 상수 FIRE_HASH는 "FIRE"이라는 문자열을 해쉬형으로 바꾼 value값을 가진 해쉬임
+    // 성능 향상할때 쓸 수있으나 굳이굳이 싶기도 함
+
+    private void Awake() 
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    public WeaponInfo GetWeaponInfo()
+    {
+        return weaponInfo;
+    }
+    
+    public void Attack()
+    {
+        myAnimator.SetTrigger(FIRE_HASH);
+        GameObject newArrow = Instantiate(fireBallPrefab, fireBallSpawnPoint.position, ActiveWeapon.Instance.transform.rotation);
+        newArrow.GetComponent<Projectile>().UpdateProjectileRange(weaponInfo.weaponRange);
+        // 화살의 인스턴스화
+    }
+}
