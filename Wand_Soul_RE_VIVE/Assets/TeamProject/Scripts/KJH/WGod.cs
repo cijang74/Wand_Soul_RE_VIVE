cdd8e58e1d4aefb21f856cf869aad72201eea711
Jaehoon Kim <cijang74@naver.com>
2024-12-11 09:38:19 +0900

    스테이지1
    대화 추가

---
+++ Wand_Soul_RE_VIVE/Assets/TeamProject/Scripts/KJH/WGod.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WGod : MonoBehaviour
+{
+    public void DestroyHer()
+    {
+        Destroy(gameObject);
+    }
+}
