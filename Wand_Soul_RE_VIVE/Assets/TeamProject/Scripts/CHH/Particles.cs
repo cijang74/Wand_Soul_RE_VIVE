88c9d198e2b22d251d770c93a6792678356a0cc1
Jaehoon Kim <cijang74@naver.com>
2024-12-12 20:56:44 +0900

    나무 공격 데미지 연결

--- Wand_Soul_RE_VIVE/Assets/TeamProject/Scripts/CHH/Particles.cs
+++ Wand_Soul_RE_VIVE/Assets/TeamProject/Scripts/CHH/Particles.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
-
+    [SerializeField] private int damageAmount;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,9 @@ public class Particle : MonoBehaviour
         Debug.Log(123);
         //40번 정도 맞음, 강화는 30번 조금 넘게 맞음
         //적 체력 깎는 함수
-
+        EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+        // enemy.Health가 존재한다면 공격력을 1이라고 가정, TakeDamage 함수에 1을 전달
+        enemyHealth?.TakeDamage(damageAmount);
     }
 
     // Update is called once per frame
