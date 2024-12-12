b589c9c5526ca3765a0ece3c3dc8d49df75a8810
Jaehoon Kim <cijang74@naver.com>
2024-12-12 21:21:45 +0900

    몬스터 데미지 변수화

--- Wand_Soul_RE_VIVE/Assets/TeamProject/Scripts/KJH/EnemyDamageSource.cs
+++ Wand_Soul_RE_VIVE/Assets/TeamProject/Scripts/KJH/EnemyDamageSource.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class EnemyDamageSource : MonoBehaviour
 {
+    [SerializeField] private int DamageAmount = 1;
+
     private void OnTriggerEnter2D(Collider2D other) // other과 충돌하면 실행되는 메서드
     {
         PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
-        player?.TakeDamage(1, transform);
+        player?.TakeDamage(DamageAmount, transform);
     }
 }
